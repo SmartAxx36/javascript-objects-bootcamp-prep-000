8d358fab402774ea80ebd40b92aa4c7f4398840a
Gavin Moore <gavizoid16@gmail.com>
2018-08-21 01:07:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   Slowdive: "Alison",
   'My Bloody Valentine': "Sometimes"
 }
-function updatePlaylist(playlist.{Phil Ochs}, ) {
+function updatePlaylist(playlist.{Phil Ochs}, {}) {
   var 'Phil Ochs' = "Here's to the State of Mississippi"
   return playlist
 }
